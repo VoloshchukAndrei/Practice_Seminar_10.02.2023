81a80b90a4ec43292b2f8aa2e35f47f5ac395f6f
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-10 13:34:22 +0500

    min & max

---
+++ minAndMax/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+if (numberA > numberB)
+{
+    Console.Write("Минимальное число: " + numberA + "Максимальное число: " + numberB);
+}
+else
+{
+    Console.Write("Минимальное число: " + numberB + "Максимальное число: " + numberA);
+}
