2ebdfd6c93c39e07dd276c6d8623ec04b40adf8d
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-10 13:05:07 +0500

    Square - сокращение кода

--- Square/Program.cs
+++ Square/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите Ваше число: ");
 int numberA = int.Parse(Console.ReadLine()!);
 int Sum = numberA * numberA;
-Console.Write("Квадрат числа: ");
+Console.Write("Квадрат числа: " ); // + Sum - в скобках сокращение кода
 Console.WriteLine(Sum);
